d9fa54fabee27bf8bfcca312fd5a1a725c15ff32
GraydeeSi <wiseguy0002@gmail.com>
2016-09-09 17:41:56 -0400

    Added not quite working bot enemies.
    I would rather someone else add the rest. I like items.

---
+++ SpiritMod/NPCs/Ki11B0t.cs
@@ -0,0 +1,37 @@
+using Terraria;
+using Terraria.ID;
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+
+namespace SpiritMod.NPCs
+{
+    public class Ki11B0t : ModNPC
+    {
+        public override void SetDefaults()
+        {
+            npc.name = "Ki11B0t";
+            npc.displayName = "Ki11B0t";
+            npc.width = 44;
+            npc.height = 56;
+            npc.damage = 29;
+            npc.defense = 8;
+            npc.lifeMax = 500;
+            npc.soundHit = 2;
+            npc.soundKilled = 2;
+            npc.value = 60f;
+            npc.knockBackResist = .95f;
+           npc.aiStyle = 3;
+            Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.AngryBones];
+            aiType = NPCID.AngryBones;
+            animationType = NPCID.AngryBones;
+        }
+        public override void NPCLoot()
+		{
+			NPC.NewNPC((int)npc.position.X + Main.rand.Next(-30,30), (int)npc.position.Y, mod.NPCType("Ki11H3ad"));
+			NPC.NewNPC((int)npc.position.X + Main.rand.Next(-30,30), (int)npc.position.Y, mod.NPCType("Ki11H3ad"));
+			NPC.NewNPC((int)npc.position.X + Main.rand.Next(-30,30), (int)npc.position.Y, mod.NPCType("Ki11H3ad"));
+		}
+    }
+}
---
+++ SpiritMod/NPCs/Ki77H3ad.cs
@@ -0,0 +1,30 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace SpiritMod.NPCs
+{
+    public class Ki11H3ad : ModNPC
+    {
+        public override void SetDefaults()
+        {
+            npc.name = "Ki11H3ad";
+            npc.displayName = "Ki11H3ad";
+            npc.width = 17;
+            npc.height = 21;
+            npc.damage = 39;
+            npc.defense = 8;
+            npc.lifeMax = 200;
+            npc.soundHit = 1;
+            npc.soundKilled = 1;
+            npc.value = 60f;
+            npc.knockBackResist = .30f;
+            npc.aiStyle = 85;
+            Main.npcFrameCount[npc.type] = Main.npcFrameCount[85];
+            aiType = 85;
+            animationType = 85;
+        }
+
+    }
+}
