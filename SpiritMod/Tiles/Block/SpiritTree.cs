9fe8a79817692527ed6093e0c8fcf7ebcd42ec78
GraydeeSi <wiseguy0002@gmail.com>
2016-09-13 15:33:48 -0400

    Updated to 1.3.3

--- SpiritMod/Tiles/Block/SpiritTree.cs
+++ SpiritMod/Tiles/Block/SpiritTree.cs
@@ -33,12 +33,12 @@ namespace SpiritMod.Tiles.Block
 			return mod.GetTexture("Tiles/Block/SpiritTree");
 		}
 
-		public override Texture2D GetTopTextures()
+		public override Texture2D GetTopTextures(int i, int j, ref int frame, ref int frameWidth, ref int frameHeight, ref int xOffsetLeft, ref int yOffset)
 		{
 			return mod.GetTexture("Tiles/Block/SpiritTree_Tops");
 		}
 
-		public override Texture2D GetBranchTextures()
+		public override Texture2D GetBranchTextures(int i, int j, int trunkOffset, ref int frame)
 		{
 			return mod.GetTexture("Tiles/Block/SpiritTree_Branches");
 		}
