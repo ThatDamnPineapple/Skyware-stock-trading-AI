ea6ffab5a732e96cd15c4f4e2c108befdc62b15e
PhoenixBladez <aryanandabhay@gmail.com>
2016-09-15 19:01:17 -0700

    New Enemy

---
+++ NPCs/LostMime.cs
@@ -0,0 +1,53 @@
+using Terraria;
+using System;
+using Terraria.ID;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+
+namespace SpiritMod.NPCs
+{
+    public class LostMime : ModNPC
+    {
+        public override void SetDefaults()
+        {
+            npc.name = "Lost Mime";
+            npc.displayName = "Lost Mime";
+            npc.width = 24;
+            npc.height = 42;
+            npc.damage = 56;
+            npc.defense = 16;
+            npc.lifeMax = 180;
+            npc.soundHit = 1;
+            npc.soundKilled = 1;
+            npc.value = 60f;
+            npc.knockBackResist = .65f;
+            npc.aiStyle = 3;
+            aiType = NPCID.Psycho;
+            Main.npcFrameCount[npc.type] = 17;
+
+        }
+        public override float CanSpawn(NPCSpawnInfo spawnInfo)
+        {
+            int x = spawnInfo.spawnTileX;
+            int y = spawnInfo.spawnTileY;
+            int tile = (int)Main.tile[x, y].type;
+            return (tile == 1) && spawnInfo.spawnTileY > Main.rockLayer ? 0.1f : 0f;
+        }
+        public override void HitEffect(int hitDirection, double damage)
+        {
+            for (int i = 0; i < 10; i++) ;
+        }
+        public override void FindFrame(int frameHeight)
+        {
+            npc.frameCounter += 0.15f;
+            npc.frameCounter %= Main.npcFrameCount[npc.type];
+            int frame = (int)npc.frameCounter;
+            npc.frame.Y = frame * frameHeight;
+        }
+        public override void AI()
+        {
+            npc.spriteDirection = npc.direction;
+        }
+    }
+}
