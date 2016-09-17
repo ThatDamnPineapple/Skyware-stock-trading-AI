ee6ddc9bc09bb8567795bf5ee7ce2d73d0e6e006
GraydeeSi <wiseguy0002@gmail.com>
2016-09-17 15:19:21 -0400

    Added storm phaser

---
+++ Items/Weapon/Gun/StormPhaser.cs
@@ -0,0 +1,52 @@
+using Terraria;
+using System;
+using Terraria.ID;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+
+namespace SpiritMod.Items.Weapon.Gun
+{
+    public class StormPhaser : ModItem
+    {private Vector2 newVect;
+        public override void SetDefaults()
+        {
+            item.name = "Storm Phaser";  
+            item.damage = 42;  
+            item.ranged = true;   
+            item.width = 65;     
+            item.height = 21;    
+            item.useTime = 35;
+            item.useAnimation = 35;
+            item.useStyle = 5;    
+            item.noMelee = true; 
+            item.knockBack = 6;
+            item.value = 100000;
+            item.rare = 6;
+            item.useSound = 36;
+            item.autoReuse = true;
+            item.shoot = 10; 
+            item.shootSpeed = 7f;
+            item.useAmmo = ProjectileID.Bullet;
+        }
+		
+		            public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+			Vector2 origVect = new Vector2(speedX, speedY);
+			for (int X = 0; X <= 7; X++)
+			{
+				if (Main.rand.Next(2) == 1)
+				{
+					newVect = origVect.RotatedBy(System.Math.PI / (Main.rand.Next(72, 1800) / 10));
+				}
+				else
+				{
+					newVect = origVect.RotatedBy(-System.Math.PI / (Main.rand.Next(72, 1800) / 10));
+				}
+			Projectile.NewProjectile(position.X, position.Y, newVect.X, newVect.Y, type, damage, knockBack, player.whoAmI);
+			}
+			return false;
+        }
+		
+    }
+}
