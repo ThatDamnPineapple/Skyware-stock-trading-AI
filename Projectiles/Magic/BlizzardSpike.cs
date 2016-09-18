3d8f59464ef5cb9173429dab809657a39602fb83
GraydeeSi <wiseguy0002@gmail.com>
2016-09-18 15:28:02 -0400

    Blizzard sceptor

---
+++ Projectiles/Magic/BlizzardSpike.cs
@@ -0,0 +1,40 @@
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace SpiritMod.Projectiles.Magic
+{
+	public class BlizzardSpike : ModProjectile
+	{
+		public override void SetDefaults()
+		{
+			projectile.name = "Icicle";
+			projectile.width = 12;
+			projectile.height = 30;
+			projectile.timeLeft = 80;
+			projectile.hostile = false;
+			projectile.friendly = true;
+			projectile.ignoreWater = true;
+			projectile.extraUpdates = 1;
+			Main.projFrames[projectile.type] = 5;
+		}
+		public override void AI()
+		{
+			projectile.frameCounter++;
+			if (projectile.frameCounter >= 6)
+			{
+				projectile.frame++;
+				projectile.frameCounter = 0;
+				if (projectile.frame >= 5)
+				{
+					projectile.frame = 0;
+				}
+			}
+		}
+		public override void Kill(int timeLeft)
+		{
+			Main.PlaySound(2, (int)projectile.position.X, (int)projectile.position.Y, 27);
+		}
+	}
+}
