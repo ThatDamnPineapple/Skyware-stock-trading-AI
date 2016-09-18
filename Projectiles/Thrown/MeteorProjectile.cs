f546556ed0385950a735c7c91f719c998b283461
GraydeeSi <wiseguy0002@gmail.com>
2016-09-18 14:26:53 -0400

    Added meteor

---
+++ Projectiles/Thrown/MeteorProjectile.cs
@@ -0,0 +1,49 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SpiritMod.Projectiles.Thrown
+{
+	public class MeteorProjectile : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.name = "Meteor";         
+            projectile.width = 30;
+			projectile.penetrate = 1;
+			projectile.timeLeft = 120;
+            projectile.height = 30;
+        }
+		 public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+                target.AddBuff(BuffID.OnFire, 200, true);           
+        }
+       	public override void Kill(int timeLeft)
+		{
+			 int proj = Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y + 20, 0, 0, mod.ProjectileType("SolarExplosion"), (int)(projectile.damage), 0, Main.myPlayer);
+			Main.PlaySound(2, (int)projectile.position.X, (int)projectile.position.Y, 14);
+		}
+		public override void AI()
+		{
+						 if (Main.rand.Next(2) == 0)
+            {
+                Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, 6, projectile.velocity.X * 0.5f, projectile.velocity.Y * 0.5f);
+            }
+		}
+
+        //public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        //{
+        //    Vector2 drawOrigin = new Vector2(Main.projectileTexture[projectile.type].Width * 0.5f, projectile.height * 0.5f);
+        //    for (int k = 0; k < projectile.oldPos.Length; k++)
+        //    {
+        //        Vector2 drawPos = projectile.oldPos[k] - Main.screenPosition + drawOrigin + new Vector2(0f, projectile.gfxOffY);
+        //        Color color = projectile.GetAlpha(lightColor) * ((float)(projectile.oldPos.Length - k) / (float)projectile.oldPos.Length);
+        //        spriteBatch.Draw(Main.projectileTexture[projectile.type], drawPos, null, color, projectile.rotation, drawOrigin, projectile.scale, SpriteEffects.None, 0f);
+        //    }
+        //    return true;
+        //}
+    }
+}
