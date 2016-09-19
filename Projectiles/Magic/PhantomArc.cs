3e7ddfe0817ae024c79399be04319f6e7f3a4ef8
Iriazul <iriazuliriazul029@gmail.com>
2016-09-19 22:50:02 +0200

    Fix_03

--- Projectiles/Magic/PhantomArc.cs
+++ Projectiles/Magic/PhantomArc.cs
@@ -27,7 +27,7 @@ namespace SpiritMod.Projectiles.Magic
 
         public override bool PreAI()
         {
-			
+
             Vector2? vector68 = null;
             if (projectile.velocity.HasNaNs() || projectile.velocity == Vector2.Zero)
             {
@@ -58,7 +58,7 @@ namespace SpiritMod.Projectiles.Magic
             }
             num811 = 2f;
             scaleFactor7 = 0f;
-            
+
             float[] array3 = new float[(int)num811];
             int num812 = 0;
             while ((float)num812 < num811)
@@ -116,34 +116,7 @@ namespace SpiritMod.Projectiles.Magic
             Utils.PlotTileLine(projectile.Center, projectile.Center + projectile.velocity * projectile.localAI[1], projectile.width * projectile.scale, new Utils.PerLinePoint(DelegateMethods.CastLight));
 
             return false;
-=======
-		public override bool PreAI()
-		{
-			Player player = Main.player[projectile.owner];
-			bool flag = player.CheckMana(player.inventory[player.selectedItem].mana, true, false);
-			bool flag2 = player.channel && flag && !player.noItems && !player.CCed;
-			if (flag2)
-			{
-				if (projectile.ai[0] == 1f)
-				{
-					Vector2 center = projectile.Center;
-					Vector2 vector = Vector2.Normalize(projectile.velocity);
-					if (float.IsNaN(vector.X) || float.IsNaN(vector.Y))
-					{
-						vector = -Vector2.UnitY;
-					}
-					int damage = projectile.damage;
-					Terraria.Projectile.NewProjectile(center.X, center.Y, vector.X, vector.Y, mod.ProjectileType("UnscratchedWispBeam_Friendly"), damage, projectile.knockBack, projectile.owner, 0f, (float)projectile.whoAmI);
-					projectile.netUpdate = true;
-				}
-			}
-			else
-			{
-				projectile.Kill();
-			}
-			return false;
->>>>>>> origin/master
-		}
+        }
 
         public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
         {
--- Projectiles/Magic/PhantomArcHandle.cs
+++ Projectiles/Magic/PhantomArcHandle.cs
@@ -19,7 +19,7 @@ namespace SpiritMod.Projectiles.Magic
 			projectile.ignoreWater = true;
 		}
 
-		public override bool PreAI()
+        public override bool PreAI()
         {
             Player player = Main.player[projectile.owner];
             float num = 1.57079637f;
@@ -75,7 +75,7 @@ namespace SpiritMod.Projectiles.Magic
                     projectile.velocity = vector11;
                 }
             }
-            
+
             int num28 = (projectile.ai[0] < 120f) ? 4 : 1;
             if (projectile.soundDelay <= 0)
             {
@@ -127,50 +127,6 @@ namespace SpiritMod.Projectiles.Magic
             player.itemAnimation = 2;
             player.itemRotation = (float)Math.Atan2((double)(projectile.velocity.Y * (float)projectile.direction), (double)(projectile.velocity.X * (float)projectile.direction));
             return false;
-=======
-		{
-			Player player = Main.player[projectile.owner];
-			float num = 1.57079637f;
-			player.RotatedRelativePoint(player.MountedCenter, true);
-			projectile.damage = (int)((float)player.inventory[player.selectedItem].damage * player.magicDamage);
-			projectile.ai[0] += 1f;
-			projectile.ai[1] += 1f;
-			projectile.frameCounter++;
-			float arg_B0_0 = projectile.ai[0];
-			if (Main.myPlayer == projectile.owner)
-			{
-				bool flag = player.CheckMana(player.inventory[player.selectedItem].mana, true, false);
-				bool flag2 = player.channel && flag && !player.noItems && !player.CCed;
-				if (flag2)
-				{
-					if (projectile.ai[0] == 1f)
-					{
-						Vector2 arg_123_0 = projectile.Center;
-						Vector2 vector = Vector2.Normalize(projectile.velocity);
-						if (float.IsNaN(vector.X) || float.IsNaN(vector.Y))
-						{
-							vector = -Vector2.UnitY;
-						}
-						int arg_169_0 = projectile.damage;
-						projectile.netUpdate = true;
-					}
-				}
-				else
-				{
-					projectile.Kill();
-				}
-			}
-			projectile.position = player.RotatedRelativePoint(player.MountedCenter, true) - projectile.Size / 2f;
-			projectile.rotation = Utils.ToRotation(projectile.velocity) + num;
-			projectile.spriteDirection = projectile.direction;
-			projectile.timeLeft = 2;
-			player.ChangeDir(projectile.direction);
-			player.heldProj = projectile.whoAmI;
-			player.itemTime = 2;
-			player.itemAnimation = 2;
-			player.itemRotation = (float)Math.Atan2((double)(projectile.velocity.Y * (float)projectile.direction), (double)(projectile.velocity.X * (float)projectile.direction));
-			return false;
->>>>>>> origin/master
-		}
+        }
 	}
 }
