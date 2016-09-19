2e76d9c8498258868a08775afff5748ac71707a1
EchoNex <hasohasacz@gmail.com>
2016-09-19 16:01:21 +0200

    CrimBullet + VileBullet

---
+++ Projectiles/Bullet/CrimBullet.cs
@@ -0,0 +1,36 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SpiritMod.Projectiles.Bullet
+{
+	public class CrimBullet : ModProjectile
+    {
+        //Warning : it's not my code. It's exampleMod code. so i donnt fully understand it
+        public override void SetDefaults()
+        {
+            projectile.name = "Crim Bullet";
+            projectile.width = 2;
+            projectile.height = 16;
+            projectile.aiStyle = 1;
+            projectile.friendly = true;
+            projectile.ranged = true;
+            projectile.penetrate = 1;
+            projectile.timeLeft = 240;
+            aiType = ProjectileID.Bullet;
+            ProjectileID.Sets.Homing[projectile.type] = true;
+        }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            if (Main.rand.Next(100) <= 9)
+            {
+                Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, 0f, 0f, 305, 0, 0f, projectile.owner, projectile.owner, Main.rand.Next(1, 3));
+            }
+        }
+
+
+    }
+}
---
+++ Projectiles/Bullet/VileBullet.cs
@@ -0,0 +1,77 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SpiritMod.Projectiles.Bullet
+{
+	public class VileBullet : ModProjectile
+    {
+        //Warning : it's not my code. It's exampleMod code. so i donnt fully understand it
+        public override void SetDefaults()
+        {
+            projectile.name = "Vile Bullet";
+            projectile.width = 2;
+            projectile.height = 16;
+            projectile.aiStyle = 1;
+            projectile.friendly = true;
+            projectile.ranged = true;
+            projectile.penetrate = 1;
+            projectile.timeLeft = 240;
+            aiType = ProjectileID.Bullet;
+            ProjectileID.Sets.Homing[projectile.type] = true;
+        }
+        public override void AI()
+        {
+            if (projectile.alpha > 70)
+            {
+                projectile.alpha -= 15;
+                if (projectile.alpha < 70)
+                {
+                    projectile.alpha = 70;
+                }
+            }
+            if (projectile.localAI[0] == 0f)
+            {
+                AdjustMagnitude(ref projectile.velocity);
+                projectile.localAI[0] = 1f;
+            }
+            Vector2 move = Vector2.Zero;
+            float distance = 400f;
+            bool target = false;
+            for (int k = 0; k < 200; k++)
+            {
+                if (Main.npc[k].active && !Main.npc[k].dontTakeDamage && !Main.npc[k].friendly && Main.npc[k].lifeMax > 5)
+                {
+                    Vector2 newMove = Main.npc[k].Center - projectile.Center;
+                    float distanceTo = (float)Math.Sqrt(newMove.X * newMove.X + newMove.Y * newMove.Y);
+                    if (distanceTo < distance)
+                    {
+                        move = newMove;
+                        distance = distanceTo;
+                        target = true;
+                    }
+                }
+            }
+            if (target)
+            {
+                AdjustMagnitude(ref move);
+                projectile.velocity = (10 * projectile.velocity + move) / 3f;
+                AdjustMagnitude(ref projectile.velocity);
+            }
+        }
+
+        private void AdjustMagnitude(ref Vector2 vector)
+        {
+            float magnitude = (float)Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
+            if (magnitude > 6f)
+            {
+                vector *= 6f / magnitude;
+            }
+        }
+
+
+    }
+}
