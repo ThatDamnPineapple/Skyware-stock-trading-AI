d000b1cfdc5672972bb4e7736e18d2d025a08676
GraydeeSi <wiseguy0002@gmail.com>
2016-09-24 15:58:52 -0400

    Bottled phantom

---
+++ Items/Accessory/BottledPhantom.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SpiritMod.Items.Accessory
+{
+	public class BottledPhantom : ModItem
+	{
+		private int proj2;
+		public override void SetDefaults()
+		{
+			item.name = "Bottled Phantom";
+            item.width = 18;
+            item.height = 18;
+            item.toolTip = "A spectral entity guides you";
+			item.value = Item.buyPrice(0, 10, 0, 0);
+			item.rare = 9;
+
+			item.accessory = true;
+			item.defense = 0;
+		}
+
+		public override void UpdateAccessory(Player player, bool hideVisual)
+		{   
+		Projectile newProj2 = Main.projectile[proj2];
+			player.GetModPlayer<MyPlayer>(mod).Phantom = true;
+			if (newProj2.active == false)
+			{
+				proj2 = Projectile.NewProjectile(player.position.X, player.position.Y, 0, 0, mod.ProjectileType("PhantomMinion"), 66, 1, player.whoAmI);
+				newProj2 = Main.projectile[proj2];
+			}
+		}
+	}
+}
