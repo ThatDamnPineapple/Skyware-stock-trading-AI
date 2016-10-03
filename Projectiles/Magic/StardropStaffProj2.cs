45584fe353b522b72bcde5b10c4f6635d7ff8354
Zocklukas <zocklukas04@gmail.com>
2016-10-04 00:55:09 +0200

    Added Stardrop Staff

---
+++ Projectiles/Magic/StardropStaffProj2.cs
@@ -0,0 +1,49 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SpiritMod.Projectiles.Magic
+{
+    public class StardropStaffProj2 : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.name = "Stardrop Staff Projectile";
+            projectile.width = 4;
+            projectile.height = 4;
+            projectile.hide = true;
+            projectile.friendly = true;
+            projectile.penetrate = -1;
+            projectile.timeLeft = 60;
+            projectile.tileCollide = false;
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                int dust = Dust.NewDust(projectile.position, projectile.width, projectile.height, 33);
+            }
+            Main.PlaySound(0, (int)projectile.position.X, (int)projectile.position.Y);
+
+        }
+
+        public override void AI()
+        {
+            int timer = 0;
+            projectile.velocity *= 0.95f;
+            timer++;
+
+            for (int i = 1; i <= 2; i++)
+            {
+                int num1 = Dust.NewDust(projectile.position, projectile.width, projectile.height, 33, projectile.velocity.X, projectile.velocity.Y, 0, default(Color), 2f
+   );
+                Main.dust[num1].noGravity = true;
+                Main.dust[num1].velocity *= 0.1f;
+            }
+            Lighting.AddLight(projectile.position, 0.4f, 0.1f, 0.1f);
+        }
+    }
+}
