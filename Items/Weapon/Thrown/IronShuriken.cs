70f5d579403585b57390e94fbf36cc5e4a0d6c27
Zocklukas <zocklukas04@gmail.com>
2016-10-15 19:05:37 +0200

    Update IronShuriken.cs

--- Items/Weapon/Thrown/IronShuriken.cs
+++ Items/Weapon/Thrown/IronShuriken.cs
@@ -17,7 +17,7 @@ namespace SpiritMod.Items.Weapon.Thrown
             item.useAnimation = 16;
             item.useTime = 16;
             item.shootSpeed = 9f;
-            item.damage = 16;
+            item.damage = 10;
             item.knockBack = 0f;
 			item.value = Terraria.Item.buyPrice(0, 0, 0, 30);
             item.crit = 5;
