ea04d815661b12ee08a9653c5fcf175d8ec0355a
GraydeeSi <wiseguy0002@gmail.com>
2016-10-18 22:31:50 -0400

    Better rune blade

--- Items/Weapon/Swung/RunicSword.cs
+++ Items/Weapon/Swung/RunicSword.cs
@@ -10,6 +10,10 @@ namespace SpiritMod.Items.Weapon.Swung
 {
     public class RunicSword : ModItem
     {
+		float downX;
+			  float downY;
+			   float upX;
+			  float upY;
         public override void SetDefaults()
         {
             item.name = "Runic Sword";     
@@ -30,8 +34,33 @@ namespace SpiritMod.Items.Weapon.Swung
 			item.value = Item.sellPrice(0, 1, 0, 0);
             item.useTurn = true;
             item.crit = 0;
-            item.shoot = mod.ProjectileType("SwordRuneProjectile");
+            item.shoot = mod.ProjectileType("Rune");
             item.shootSpeed = 4f;
+        }
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+            float SdirX = (float)(Main.MouseWorld.X - player.position.X);
+            float SdirY = (float)(Main.MouseWorld.Y - player.position.Y);
+            float angleup = (float)Math.Atan(SdirX / SdirY) + 25;
+			 float angledown = (float)Math.Atan(SdirX / SdirY) - 25;
+			 if (SdirY < 0)
+			 {
+			 downX = (float)(0 - (Math.Sin(angledown) * 7.5));
+			 downY = (float)(0 - (Math.Cos(angledown) * 7.5));
+			   upX = (float)(0 - (Math.Sin(angleup) * 7.5));
+			  upY = (float)(0 - (Math.Cos(angleup) * 7.5));
+			 }
+			 
+			 if (SdirY > 0)
+			 {
+			  downX = (float)(Math.Sin(angledown) * 7.5);
+			  downY = (float)(Math.Cos(angledown) * 7.5);
+			  upX = (float)(Math.Sin(angleup) * 7.5);
+			  upY = (float)(Math.Cos(angleup) * 7.5);
+			 }
+			Terraria.Projectile.NewProjectile(position.X, position.Y, downX, downY, type, damage, knockBack, player.whoAmI, 0f, 0f);
+			Terraria.Projectile.NewProjectile(position.X, position.Y, upX, upY, type, damage, knockBack, player.whoAmI, 0f, 0f);
+            return false;
         }
         public override void AddRecipes()
         {
