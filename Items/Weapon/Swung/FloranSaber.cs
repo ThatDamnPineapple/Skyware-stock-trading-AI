6f8c8ccecd0c8afe293c6174be00210d7155b44a
Jenosis <benmasai---1@hotmail.com>
2017-01-13 16:42:15 -0500

    fixed namespace

--- Items/Weapon/Swung/FloranSaber.cs
+++ Items/Weapon/Swung/FloranSaber.cs
@@ -4,7 +4,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace SpiritMod.Items.Weapon.Returning
+namespace SpiritMod.Items.Weapon.Swung
 {
 	public class FloranSaber : ModItem
 	{
