e3732262e8f807e890b1363cc9c060d8d8b89ad9
Jenosis <benmasai---1@hotmail.com>
2017-01-13 16:43:02 -0500

    fixed namespace

--- Items/Weapon/Bow/GeodeBow.cs
+++ Items/Weapon/Bow/GeodeBow.cs
@@ -3,7 +3,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace SpiritMod.Items.Weapon.Swung
+namespace SpiritMod.Items.Weapon.Bow
 {
     public class GeodeBow : ModItem
     {
