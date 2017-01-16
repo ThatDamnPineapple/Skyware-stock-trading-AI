5e023c49dcb37c124fc2dbd2cb473a9e4c0ac0fd
Jenosis <benmasai---1@hotmail.com>
2017-01-16 16:41:29 -0500

    Add files via upload

--- Items/Weapon/Spear/Talonginus.cs
+++ Items/Weapon/Spear/Talonginus.cs
@@ -1,17 +1,13 @@
 using System;
-
 using Microsoft.Xna.Framework;
-
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-
 namespace SpiritMod.Items.Weapon.Spear
 {
     public class Talonginus : ModItem
     {
         int currentHit;
-
         public override void SetDefaults()
         {
             item.name = "Talonginus";
@@ -19,37 +15,28 @@ namespace SpiritMod.Items.Weapon.Spear
             item.height = 24;
             item.value = Item.sellPrice(0, 1, 30, 0);
             item.rare = 3;
-
             item.crit = 6;
-            item.damage = 21;
+            item.damage = 15;
             item.knockBack = 6f;
-
             item.useStyle = 5;
             item.useTime = 7;
             item.useAnimation = 7;
-
             item.melee = true;
             item.expert = true;
             item.noMelee = true;
             item.autoReuse = true;
             item.noUseGraphic = true;
-
             item.shoot = mod.ProjectileType("TalonginusProj");
             item.shootSpeed = 9f;
-
             item.UseSound = SoundID.Item1;
-
             this.currentHit = 0;
         }
-
         public override bool CanUseItem(Player player)
         {
             if (player.ownedProjectileCounts[item.shoot] > 0)
                 return false;
-
             return true;
         }
-
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
 		{
 			//create velocity vectors for the two angled projectiles (outwards at PI/15 radians)
@@ -65,7 +52,6 @@ namespace SpiritMod.Items.Weapon.Spear
 				}
 			speedX = newVect.X;
 			speedY = newVect.Y;
-
             this.currentHit++;
             return true;
 		}    
