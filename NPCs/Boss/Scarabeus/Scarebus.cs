5db769f079372eb4bf6c2bcd6a804fcfb31a04ee
PhoenixBladez <aryanandabhay@gmail.com>
2017-01-29 15:59:45 -0800

    FIXED MY DUMBASS

--- NPCs/Boss/Scarabeus/Scarebus.cs
+++ NPCs/Boss/Scarabeus/Scarebus.cs
@@ -81,7 +81,7 @@ namespace SpiritMod.NPCs.Boss.Scarabeus
 				npc.ai[0]++;
 				}
 				npc.noGravity = false;
-				npc.noTileCollide = true;
+				npc.noTileCollide = false;
 				if (Main.rand.Next(2) > 0)
 				{
 				npc.velocity.X = npc.ai[2] * 0.1f;
