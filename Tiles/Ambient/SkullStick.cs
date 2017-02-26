66cc4afff5d48f92662704e5a601b7d2404b5008
GraydeeSi <wiseguy0002@gmail.com>
2017-02-25 20:42:46 -0500

    Work in progress reach

---
+++ Tiles/Ambient/SkullStick.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.Enums;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Terraria.DataStructures;
+
+namespace SpiritMod.Tiles.Ambient
+{
+    public class SkullStick : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style1xX);
+            TileObjectData.addTile(Type);
+            TileObjectData.newTile.Height = 3;
+            TileObjectData.newTile.CoordinateHeights = new int[]
+            {
+            16,
+            16,
+            16
+            };
+            AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTorch);
+            AddMapEntry(new Color(200, 200, 200), "Skull Stick");
+            adjTiles = new int[] { TileID.Lamps };
+        }
+
+
+    }
+}
