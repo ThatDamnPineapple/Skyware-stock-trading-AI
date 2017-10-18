aaa68c0d499df3a5cd353ec929c1640d139632ce
Lordcakespy <31828944+Lordcakespy@users.noreply.github.com>
2017-10-18 12:01:27 -0400

    all NPCs now have at least 2 ToT lines

--- NPCs/_GNPC.cs
+++ NPCs/_GNPC.cs
@@ -414,36 +414,42 @@ namespace SpiritMod.NPCs
 				case NPCID.Merchant:
 					if (dialogue == 0)
 						return "Oh, here's some candy. You have no idea how hard it was to get this.";
-					else
+					else if (dialogue == 1)
 						return "I can be greedy, but I like to be festive. Here you go!";
+					else
+						return "I'll give you this piece for free. I can't give you any promises about the next piece.";
 				case NPCID.Nurse:
 					if (dialogue == 0)
-						return "Oh here, kiddo. Make sure there aren't any razorblades in there!";
-					else
+						return "Here, kiddo. Make sure there aren't any razorblades in there!";
+					else if (dialogue == 1)
 						return "It may not be the healithiest option, but candy is pretty nice. Take some.";
+					else
+						return "I'm pretty sure this candy that makes you healthier. Maybe. Don't quote me on this."; 
 				case NPCID.ArmsDealer:
 					if (dialogue == 0)
 						if (player.player.HeldItem.type == ItemID.CandyCornRifle)
-							return "Is that... it is! A Candy Corn Rifle. Here, I want you to have this for showing it to me.";
+							return "Is that... it is! A Candy Corn Rifle! Here, I want you to have this for showing it to me.";
 						else
 							return "I hear there is a gun that shoots candy. Oh what I wouldn't give for one. What? Oh, yes, here's your candy.";
 					else
-						return "You wouldn't believe it. I asked for ammo, but these guys gave me candy instead! You want a piece?";
+						return "You wouldn't believe it. I asked for ammo, but my suppliers gave me candy instead! You want a piece?";
 				case NPCID.Dryad:
 					if (dialogue == 0)
 						return "Do you have any idea what's in that candy? Here, this stuff is much better for you. I made it myself.";
-					else
+					else if (dialogue == 1)
 						return "Is it that time of year again? Time flies by so fast when you are as old as I am... Oh, here, have some candy.";
+					else
+						return "I wish I had candy seeds to sell you. Growing sweets would be far more sustainable than going door-to-door asking for them.";
 				case (NPCID.Guide):
 					if (dialogue == 0)
 						return "Here. You may collect one piece of candy a night from every villager during halloween.";
 					else
-						return "Of course. Candy can be used during any season, to get special effects.";
+						return "Candy can be used during any season to get special effects.";
 				case NPCID.Demolitionist:
 					if (dialogue == 0)
 						return "I was making a sugar rocket, and this was left over. Do you want some?";
 					else
-						return "Aye, this candy may or may not have explosives in it, I don't remember.";
+						return "Ach, this candy may or may not have explosives in it, I don't remember.";
 				case NPCID.Clothier:
 					if (dialogue == 0)
 						return "My mama always told me candy was like life. Or was it a box? ... er, something like that. Here, take a piece.";
@@ -453,29 +459,33 @@ namespace SpiritMod.NPCs
 					if (dialogue == 0)
 						return "I tried combining rocket boots and candy, but it didn't really work out. You want what's left?";
 					else
-						return "Test " + npc.TypeName;
+						return "It turns out putting together every flavor of candy tastes pretty bad. I know, I'm disappointed too.";
 				case NPCID.Wizard:
 					if (dialogue == 0)
 						return "I'm pretty sure this isn't enchanted candy, but I could make some if you want! No? Ok...";
-					else
+					else if (dialogue == 1)
 						return "I have some candy for you, but I could enchant it if you would li... No? Ok.";
+					else
+						return "Are you sure you don't want enchanted candy? It wouldn't be a bother if I just... No? Fine..."
 				case NPCID.Mechanic:
 					if (dialogue == 0)
 						return "Don't mind the hydraulic fluid on the candy. In fact, consider it extra flavor.";
+					else if (dialogue == 1)
+						return "If you keep this candy in your pocket, it can monitor your heart rate, blood pressure, and tell how many steps you take!";
 					else
-						return "Test " + npc.TypeName;
+						return "It turns out you can't make an engine powered by candy. Birds are fine, but candy? Too much, apparently."
 				case NPCID.SantaClaus:
 					if (dialogue == 0)
-						return "Something ain't right. This feels all wrong";
+						return "Something isn't right. This feels all wrong.";
 					else
-						return "Test " + npc.TypeName;
+						return "Ho ho ho! 'Tis the season -- wait, 'tisn't the season! What am I doing here?";
 				case NPCID.Truffle:
 					if (dialogue == 0)
 						return "Is this candy vegan? Of course not, you sicko!";
 					else if ((name = NPC.GetFirstNPCNameOrNull(NPCID.Nurse)) != null)
 						return name + " wanted some of these for her supply. I wonder what that was about?";
 					else
-						return "Test " + npc.TypeName;
+						return "What do you mean there's mold on this piece? That's clearly fungus!";
 				case NPCID.Steampunker:
 					if (dialogue == 0)
 						return "All hallow's eve, you say? Cor, I've got just the thing! Here, have some treacle!";
@@ -488,34 +498,36 @@ namespace SpiritMod.NPCs
 						return "It isn't about how it tastes... It's about how rich the colors look. Take a piece, why don't you?";
 				case NPCID.PartyGirl:
 					if (dialogue == 0)
-						return "I love this time of year, where people give out free candy! Here, have a piece!";
+						return "I love this time of year! Now I don't need an excuse give out free candy! Here, have a piece!";
 					else
-						return "Test " + npc.TypeName;
+						return "Who ever said you needed drugs to party? Candy is waaaay better!";
 				case NPCID.Cyborg:
 					if (dialogue == 0)
-						return "My calendar programming has exacted it is approximately Halloween; enjoy your sucrose based food!";
+						return "My calendar programming has determined that it is approximately Halloween; enjoy your sucrose based food!";
 					else
-						return "Test " + npc.TypeName;
+						return "Sugar always seems to gum up my inaards. Here, hold this candy while I try and fix that.";
 				case NPCID.Painter:
 					if (dialogue == 0)
 						return "I might've dripped a little paint on this candy, but it's probably lead-free. Hopefully.";
 					else
-						return "Test " + npc.TypeName;
+						return "Oh, "+player.player.name+", you want candy? Let me get your portrait, then you can have some.";
 				case NPCID.WitchDoctor:
 					if (dialogue == 0)
-						return "I decided not to give you lemon heads; or should I say: lemon-flavored heads, enjoy!";
+						return "I decided not to give you lemon heads... or, should I say, lemon-flavored heads. Enjoy!";
 					else
 						return "Beware, "+ player.player.name +", for it is the season of ghouls and spirits. This edible talisman will protect you.";
 				case NPCID.Pirate:
 					if (dialogue == 0)
 						return "Yo ho ho and a bottle of... candy. Take some!";
+					else if (dialogue == 1)
+						return "This candy cost me an arm and a leg. Enjoy that now, or it's to the plank with ye!";
 					else
-						return "This candy cost me an arm and a leg, you enjoy that now or its to the plank with ye!";
+						return "Arrr, there's an old sayin' that goes \"Do what ye want, 'cause a pirate is free.\" I'd like to think that applies to eatin' candy as well!"
 				case NPCID.Stylist:
 					if (dialogue == 0)
 						return "I usually save these for after haircuts, but go ahead and take a piece, darling.";
 					else
-						return "Test " + npc.TypeName;
+						return "I've got plenty of candy, hon! Take as much as you want.";
 				case NPCID.TravellingMerchant:
 					if (dialogue == 0)
 						return "I have rare candies from all over "+ Main.worldName +". Here, take some.";
@@ -530,7 +542,7 @@ namespace SpiritMod.NPCs
 						return "Dude, you don't ask a kid for candy. You just don't.";
 				case NPCID.TaxCollector:
 					if ((dialogue = Main.rand.Next(3)) == 0)
-						return "Halloween? Bah humbug. Take your candy and get outta here!";
+						return "Halloween? Bah, humbug! Take your candy and get out.";
 					else if (dialogue == 1)
 						return "You come to my door to take my sweets? Well go on then, take 'em!";
 					else
@@ -539,24 +551,24 @@ namespace SpiritMod.NPCs
 					if (dialogue == 0)
 						return "I'm feeling happy, it's my people's season! Take some candy!";
 					else
-						return "Test " + npc.TypeName;
+						return "Did you know that rock candy doesn't actually grow underground? Oh, you did? Hmph.";
 				case NPCID.DD2Bartender:
 					if (dialogue == 0)
 						return "I managed to find some ale-flavored candy! Maybe this world ain't so bad after all.";
 					else
-						return "Test " + npc.TypeName;
+						return "Wiping a counter all day has made me appreciate the little things in life, like candy. Care for a piece?";
 			}
 			if (npc.type == Adventurer._type)
 			{
 				if (dialogue == 0)
-					return "You wouldn't believe me, when I tell you I got this from a faraway kingdom made of CANDY! I promise it has an exquisite taste.";
+					return "You wouldn't believe me if I told you I got this from a faraway kingdom made of CANDY! I promise it has an exquisite taste.";
 				else
-					return "Test " + npc.TypeName;
+					return "I hear you can get more candy from the goodie bags that monsters hold. As if I needed an excuse to slay some zombies!";
 			}
 			else if (npc.type == LoneTrapper._type)
 			{
 				if (dialogue == 0)
-					return "The only thing that makes me forget my suffering is candy, I suppose you can have some.";
+					return "The only thing that makes me forget my suffering is candy. I suppose you can have some.";
 				else
 					return "Candy helps fill the aching void where my sould used to be. Maybe it can help you too.";
 			}
@@ -565,14 +577,14 @@ namespace SpiritMod.NPCs
 				if (dialogue == 0)
 					return "When I served beside the Moon Lord I was offered tempting powers, but none were more tempting than this piece of candy.";
 				else
-					return "Test " + npc.TypeName;
+					return "I was about to sacrifice this piece of candy, but then I realized I don't need to do that anymore. Do you want it?";
 			}
 			else if (npc.type == Martian._type)
 			{
 				if (dialogue == 0)
-					return "Test " + npc.TypeName;
+					return "I've determined through years of scientific analysis that this candy here is irresistible to any anyone who- hey, give it back!";
 				else
-					return "Test " + npc.TypeName;
+					return "I'm unfamiliar with this holiday of yours. Am I supposed to give only treats? Why can I not trick as well?";
 			}
 			else if (npc.type == Rogue._type)
 			{
@@ -586,12 +598,12 @@ namespace SpiritMod.NPCs
 				if (dialogue == 0)
 					return "Behold! Enchanted candy! Enchantingly tasty, that is!";
 				else
-					return "Watch closely, for I shall channel the power of ghouls and spirits to summon... Candy!";
+					return "Watch closely, for I shall channel the power of the spirits to summon... Candy!";
 			}
 			if (dialogue == 0)
 				return "Hello, " + player.player.name + ". Take some candy!";
 			else
-				return "Here. I got some candy for you.";
+				return "Here. I have some candy for you.";
 		}
 
 		public override void SetupShop(int type, Chest shop, ref int nextSlot)
