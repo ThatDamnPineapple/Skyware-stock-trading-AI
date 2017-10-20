7ab531eb10ead7b7e812a22631b70f3a764721aa
GraydeeSi <wiseguy0002@gmail.com>
2017-10-20 16:40:43 -0400

    halloween shizz

--- Items/GlobalItem.cs
+++ Items/GlobalItem.cs
@@ -217,7 +217,7 @@ namespace SpiritMod.Items
 			_ItemUtils.DropCandy(player);
 			if (Main.rand.Next(3) == 0)
 			{
-				string[] lootTable = {"MaskSchmo", "MaskGraydee", "MaskLordCake", "MaskVladimir", "MaskKachow", "MaskHulk", };
+				string[] lootTable = {"MaskSchmo", "MaskGraydee", "MaskLordCake", "MaskVladimir", "MaskKachow", "MaskHulk", "MaskBlaze", };
 			int loot = Main.rand.Next(lootTable.Length);
             player.QuickSpawnItem(mod.ItemType(lootTable[loot]));
 			}
