d52988a93f19a1aea2dc8651198b25ce7104c93f
Leemyy <yunomanics@live.de>
2018-05-27 23:12:24 +0200

    Added Wishbone's Pet

---
+++ Items/DonatorItems/DemonTail.cs
@@ -0,0 +1,45 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SpiritMod.Items.DonatorItems
+{
+	class DemonTail : ModItem
+	{
+		public static readonly int _type;
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Demon Tail");
+			Tooltip.SetDefault("~Donator Item~");
+		}
+
+		public override void SetDefaults()
+		{
+			item.UseSound = SoundID.Item2;
+			item.useAnimation = 20;
+			item.useTime = 20;
+
+			item.width = 22;
+			item.height = 32;
+
+			item.value = 20000;
+			item.rare = 5;
+			item.noMelee = true;
+
+			item.buffType = LoomingPresence._type;
+			item.shoot = Projectiles.DonatorItems.DemonicBlob._type;
+		}
+
+		public override void AddRecipes()
+		{
+			var recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.BlackLens);
+			recipe.AddIngredient(ItemID.DemonScythe);
+			recipe.AddIngredient(ItemID.WaterCandle);
+			recipe.AddIngredient(ItemID.SoulofNight, 3);
+			recipe.AddTile(TileID.TinkerersWorkbench);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
