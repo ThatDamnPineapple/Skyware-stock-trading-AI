4b572660dfcd0afa42f5418d847e8bb25f08f9dc
Leemyy <yunomanics@live.de>
2018-05-27 02:23:29 +0200

    Added Duskfeather Dagger

---
+++ Items/DonatorItems/DuskfeatherDagger.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SpiritMod.Items.DonatorItems
+{
+	class DuskfeatherDagger : ModItem
+	{
+		public static readonly int _type;
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Duskfeather Dagger");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 22;
+			item.height = 12;
+			item.useStyle = 1;
+
+			item.value = 1000000;
+			item.rare = 11;
+
+			item.damage = 180;
+			item.knockBack = 7f;
+			//item.thrown = true;
+			item.autoReuse = true;
+			item.shootSpeed = 16f;
+			item.UseSound = SoundID.Item1;
+			item.shoot = Projectiles.DonatorItems.DuskfeatherBlade._type;
+			item.noUseGraphic = true;
+			item.noMelee = true;
+
+			item.useTime = 18;
+			item.useAnimation = 18;
+		}
+
+		public override bool AltFunctionUse(Player player)
+		{
+			return true;
+		}
+
+		public override bool CanUseItem(Player player)
+		{
+			if (player.altFunctionUse == 2)
+			{
+				if (item.useStyle == 1)
+				{
+					item.useStyle = 4;
+					item.noUseGraphic = false;
+					item.UseSound = null;
+				}
+				else
+					return false;
+			}
+			else
+			{
+				if (player.ownedProjectileCounts[Projectiles.DonatorItems.DuskfeatherBlade._type] >= 8)
+					Projectiles.DonatorItems.DuskfeatherBlade.AttractOldestBlade(player);
+				item.useStyle = 1;
+				item.noUseGraphic = true;
+				item.UseSound = SoundID.Item1;
+			}
+			return true;
+		}
+
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			if (player.altFunctionUse == 2)
+			{
+				Projectiles.DonatorItems.DuskfeatherBlade.AttractBlades(player);
+				return false;
+			}
+			return true;
+		}
+
+		public override void AddRecipes()
+		{
+			var recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Muramasa);
+			recipe.AddIngredient(ItemID.Feather, 8);
+			recipe.AddIngredient(ItemID.FossilOre, 25);
+			recipe.AddIngredient(ItemID.Amber, 8);
+			recipe.AddTile(TileID.DemonAltar);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ Projectiles/DonatorItems/DuskfeatherBlade.cs
@@ -0,0 +1,237 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+using static SpiritMod.Projectiles.DonatorItems.DuskfeatherBlade.DuskfeatherState;
+
+namespace SpiritMod.Projectiles.DonatorItems
+{
+	class DuskfeatherBlade : ModProjectile
+	{
+		public static int _type;
+
+		private const float Range = 25 * 16;
+		private const float Max_Dist = 100 * 16;
+		private const int Total_Updates = 3;
+		private const int Total_Lifetime = 3600 * Total_Updates;
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Duskfeather Blade");
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.friendly = true;
+			projectile.thrown = true;
+			projectile.height = 14;
+			projectile.width = 14;
+			projectile.alpha = 255;
+			projectile.penetrate = 2;
+			projectile.aiStyle = -1;
+			projectile.extraUpdates = Total_Updates - 1;
+			projectile.timeLeft = Total_Lifetime;
+		}
+
+		internal static void AttractBlades(Player player)
+		{
+			for (int i = 0; i < Main.maxProjectiles; ++i)
+			{
+				var projectile = Main.projectile[i];
+				if (!projectile.active)
+					continue;
+				if (projectile.type == _type &&
+					projectile.owner == player.whoAmI)
+				{
+					Retract(projectile);
+				}
+			}
+		}
+
+		internal static void AttractOldestBlade(Player player)
+		{
+			Projectile oldest = null;
+			int timeLeft = int.MaxValue;
+			for (int i = 0; i < Main.maxProjectiles; ++i)
+			{
+				var projectile = Main.projectile[i];
+				if (!projectile.active)
+					continue;
+				if (projectile.type == _type &&
+					projectile.owner == player.whoAmI &&
+					(int)projectile.ai[0] != (int)Return &&
+					projectile.timeLeft < timeLeft)
+				{
+					timeLeft = projectile.timeLeft;
+					oldest = projectile;
+				}
+			}
+			if (oldest != null)
+				Retract(oldest);
+		}
+
+		internal static void Retract(Projectile projectile)
+		{
+			projectile.ai[0] = (int)Return;
+			projectile.netUpdate = true;
+		}
+
+
+		public override void Kill(int timeLeft)
+		{
+			if (projectile.alpha == 255)
+				return;
+		}
+
+		private DuskfeatherState State
+		{
+			get { return (DuskfeatherState)(int)projectile.ai[0]; }
+			set { projectile.ai[0] = (int)value; }
+		}
+		private float FiringVelocity
+		{
+			get { return projectile.ai[1]; }
+			set { projectile.ai[1] = value; }
+		}
+		private Vector2 Origin
+		{
+			get { return new Vector2(projectile.localAI[0], projectile.localAI[1]); }
+			set
+			{
+				projectile.localAI[0] = value.X;
+				projectile.localAI[1] = value.Y;
+			}
+		}
+		private float Poof
+		{
+			get { return projectile.localAI[0]; }
+			set { projectile.localAI[0] = value; }
+		}
+		public override void AI()
+		{
+			if (State != Return)
+			{
+				if (projectile.alpha > 25)
+					projectile.alpha -= 25;
+				else
+					projectile.alpha = 0;
+			}
+			switch (State)
+			{
+				case Moving:
+					AIMove();
+					break;
+				case HittingBlock:
+					break;
+				case HittingNPC:
+					break;
+				case Stopped:
+					AIStopped();
+					break;
+				case Return:
+					AIReturn();
+					break;
+			}
+		}
+
+		private void AIMove()
+		{
+			if (Origin == Vector2.Zero)
+			{
+				projectile.rotation = (float)System.Math.Atan2(projectile.velocity.X, -projectile.velocity.Y);
+				Origin = projectile.position;
+				projectile.velocity *= 1f/Total_Updates;
+				FiringVelocity = projectile.velocity.Length();
+			}
+			float distanceFromStart = Vector2.DistanceSquared(projectile.position, Origin);
+			if (Range*Range < distanceFromStart)
+			{
+				Stop();
+			}
+		}
+
+		private void AIStopped()
+		{
+			float distanceFromOwner = Vector2.DistanceSquared(projectile.position, Main.player[projectile.owner].position);
+			if (Max_Dist*Max_Dist < distanceFromOwner)
+				Retract(projectile);
+		}
+
+		private void AIReturn()
+		{
+			projectile.tileCollide = false;
+			projectile.penetrate = -1;
+			if (Poof == 0)
+			{
+				Poof = 1;
+				//Utils.PoofOfSmoke(projectile.position);
+			}
+			Vector2 velocity = Main.player[projectile.owner].MountedCenter - projectile.position;
+			float distance = velocity.Length();
+			if (distance < FiringVelocity)
+			{
+				projectile.Kill();
+				return;
+			}
+			float startFade = 10 * Total_Updates * FiringVelocity;
+			if (distance < startFade)
+				projectile.alpha = 255 - (int)(distance/startFade * 255);
+
+			velocity /= distance;
+			velocity *= FiringVelocity *
+				(distance < Range ?
+				1.5f :
+				1.5f + (distance - Range) / Range);
+			projectile.velocity = velocity;
+			projectile.rotation = (float)System.Math.Atan2(velocity.X, -velocity.Y) + (float)System.Math.PI;
+		}
+
+		private void Stop()
+		{
+			projectile.velocity = Vector2.Zero;
+			State = Stopped;
+			Poof = 0;
+		}
+
+
+		public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough)
+		{
+			width = 0;
+			height = 0;
+			return true;
+		}
+
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			if (State != 0)
+				return false;
+			Stop();//StickingToBlock
+			return false;
+		}
+
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{
+			Stop();//StickingToNPC
+		}
+
+		public override bool? CanHitNPC(NPC target)
+		{
+			var state = State;
+			return (state == Moving || state == Return) ? null : (bool?)false;
+		}
+
+		public override bool? CanCutTiles()
+		{
+			return State == Moving ? null : (bool?)false;
+		}
+
+		public enum DuskfeatherState
+		{
+			Moving = 0,
+			HittingBlock,
+			HittingNPC,
+			Stopped,
+			Return
+		}
+	}
+}
