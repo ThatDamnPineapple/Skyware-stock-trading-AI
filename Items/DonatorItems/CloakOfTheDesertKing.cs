59e886be3ae69dd566340a7ad2f946f58ea4d888
Leemyy <yunomanics@live.de>
2018-06-04 23:46:20 +0200

    Added TurkeyBacon's item

---
+++ Items/DonatorItems/CloakOfTheDesertKing.cs
@@ -0,0 +1,43 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SpiritMod.Items.DonatorItems
+{
+	class CloakOfTheDesertKing : ModItem
+	{
+		public static readonly int _type;
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Cloak of the Desert King");
+			Tooltip.SetDefault("~Donator Item~");
+		}
+
+		public override void SetDefaults()
+		{
+			item.UseSound = SoundID.Item2;
+			item.useAnimation = 20;
+			item.useTime = 20;
+
+			item.width = 22;
+			item.height = 12;
+
+			item.value = 60000;
+			item.rare = 6;
+			item.noMelee = true;
+
+			item.buffType = RabbitOfCaerbannogBuff._type;
+			item.shoot = Projectiles.DonatorItems.RabbitOfCaerbannog._type;
+		}
+
+		public override void AddRecipes()
+		{
+			var recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.CrimsonCloak);
+			recipe.AddIngredient(ItemID.SoulofNight, 10);
+			recipe.AddTile(TileID.Loom);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ Projectiles/DonatorItems/RabbitOfCaerbannog.cs
@@ -0,0 +1,39 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+using SpiritMod.Items.DonatorItems;
+
+namespace SpiritMod.Projectiles.DonatorItems
+{
+	class RabbitOfCaerbannog : ModProjectile
+	{
+		public static readonly int _type;
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Rabbit of Caerbannog");
+			Main.projFrames[projectile.type] = 7;
+			Main.projPet[projectile.type] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.netImportant = true;
+			projectile.width = 30;
+			projectile.height = 30;
+			projectile.aiStyle = 26;
+			projectile.friendly = true;
+			projectile.penetrate = -1;
+			projectile.timeLeft *= 5;
+			aiType = ProjectileID.BlackCat;
+		}
+
+		public override void AI()
+		{
+			var owner = Main.player[projectile.owner];
+			if (owner.active && owner.HasBuff(RabbitOfCaerbannogBuff._type))
+				projectile.timeLeft = 2;
+		}
+	}
+}
